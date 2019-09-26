9aaa1b99581a71a1c9a6fbe9464c6252fc193b93
zhouzhi_98 <zhouzhi_98@163.com>
2019-09-26 22:05:07 +0800

    c

--- typeidea/typeidea/blog/admin.py
+++ typeidea/typeidea/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Post, Category, Tag
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
